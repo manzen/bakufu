bcce08f8cc1312f74f2fb531bf76de24af993058
Yasunori Arakawa <arakawa@fez-inc.jp>
2019-08-24 17:08:16 +0900

    Add play button

--- front/src/view/music.js
+++ front/src/view/music.js
@@ -1,3 +1,20 @@
 function hello () {
-    alert("Hello");
+  alert("Hello");
 };
+
+const audio = new Audio();
+let isPlay = true;
+audio.src = "../../resources/sigeru.mp3";
+function togglePlay() {
+  if (isPlay) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+  isPlay = !isPlay;
+}
+function stop(){
+  audio.pause();
+  audio.currentTime = 0;
+}
+
