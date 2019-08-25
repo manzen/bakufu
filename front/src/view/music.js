f059654cc2268a627f67eaac7566e3ef99f9bea3
iemong <kiyoshidainagon@gmail.com>
2019-08-25 11:45:47 +0900

    倍速機能を追加する

--- front/src/view/music.js
+++ front/src/view/music.js
@@ -1,6 +1,7 @@
 const audio = new Audio();
 audio.src = "../../resources/ultra_soul.mp3";
-function play() {
+function play(scale = 1.0) {
+  audio.playbackRate = scale
   audio.play();
 }
 function stop(){
